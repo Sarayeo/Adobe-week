e64473097f060a34b51b5c369fa2875800070869
Sarayeo <yeokpeusseusarafiela@gmail.com>
2023-12-08 08:48:03 +0100

    almost over paging

--- frontend/AnimaPac/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { Wireframe } from "./screens/Wireframe";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<Wireframe />);
